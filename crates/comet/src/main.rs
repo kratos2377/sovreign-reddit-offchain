ef54c115a92ced90dbcf7c10c1f17e0d881de4e2
kratos2377 <shobityadav23@gmail.com>
2025-06-07 18:23:29 +0530

    Adding RabbitMQ layer

--- crates/comet/src/main.rs
+++ crates/comet/src/main.rs
@@ -5,13 +5,11 @@ use state::AppDBState;
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
 use tower_http::cors::CorsLayer;
+use tracing::info;
 
 pub mod logging_tracing;
 pub mod conf;
-pub mod kafka;
 pub mod state;
-pub mod controller;
-pub mod routes;
 pub mod error;
 pub mod payload;
 
@@ -24,20 +22,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>  {
   logging_tracing::init(&config)?;
 
 
- let kafka_producer = kafka::init_producer::create_new_kafka_producer(&config.kafka).unwrap();
-
-  let state = AppDBState { producer: kafka_producer };
-
-        let comet_routes = routes::create_comet_routes();
-   // let game_routes = routes::game_logic_routes::create_game_routes();
     let routes_all = Router::new()
                           .route( "/api/v1/health", get(health))
-                            .nest("/api/v1/comet", comet_routes)
                          //   .nest( "/api/v1/game", game_routes)
                             .layer(ServiceBuilder::new()
                                     .layer(CookieManagerLayer::new())
-                                    .layer(CorsLayer::permissive()))
-                            .with_state(state);
+                                    .layer(CorsLayer::permissive()));
 
 
     // run it
--- crates/comet/src/routes.rs
+++
@@ -1,8 +0,0 @@
-use axum::{middleware, routing::{post,get,put}, Router};
-
-use crate::{controller, state::AppDBState, };
-
-pub fn create_comet_routes() -> Router<AppDBState> {
-    Router::new()
-    .route("/publish_chain_changes", post(controller::publish_state_change_to_topic))
-}
